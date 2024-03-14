fe5f1e279b04fac46ae8aff9eab2dff7aa796df0
chingizagha <cingiz167@gmail.com>
2024-03-14 23:42:35 +0400

    Update

---
+++ GHFollowers/FavoritesListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesListViewController.swift
+//  GHFollowers
+//
+//  Created by Chingiz on 14.03.24.
+//
+
+import UIKit
+
+class FavoritesListViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+
+}
---
+++ GHFollowers/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  GHFollowers
+//
+//  Created by Chingiz on 14.03.24.
+//
+
+import UIKit
+
+class SearchViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
